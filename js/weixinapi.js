3bcc620c8aff7c2f25063ca08cf5ce8410201c70
Hao Li <haoli@Haos-MacBook-Pro.local>
2014-11-30 17:49:55 +0800

    degradation14113002

--- js/weixinapi.js
+++ js/weixinapi.js
@@ -1,8 +1,8 @@
 function onBridgeReady() {
-    var mainTitle="我们在这 我们在乎",
-        mainDesc="可口可乐中国发布2013年可持续发展报告",
-        mainURL="http://cokecn-cokecn.stor.sinaapp.com/index.html",
-        mainImgUrl= "http://cokecn-cokecn.stor.sinaapp.com/images/share_sample.png";
+    var mainTitle="2014“无能”的淘宝",
+        mainDesc="2014淘宝1212万能盛典 开启用想象力淘宝的时代",
+        mainURL="http://106.187.50.149/ali1212h5/113002/index.html",
+        mainImgUrl= "http://106.187.50.149/ali1212h5/113002/images/share_sample.png";
 
     //转发朋友圈
     WeixinJSBridge.on("menu:share:timeline", function(e) {
